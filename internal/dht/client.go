2bbdc9a526803c0dc67378617f275e2b0078ba92
Matt Wang <matthewianwang@gmail.com>
2025-05-16 12:56:56 -0700

    feat(client): add client join function

---
+++ internal/dht/client.go
@@ -0,0 +1,41 @@
+package dht
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+
+	pb "github.com/matthewwangg/distributed-kv-store/proto/node"
+)
+
+func (n *Node) ClientJoin() (map[string]string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	conn, err := grpc.NewClient(n.JoinAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		return nil, fmt.Errorf("failed to create gRPC client: %w", err)
+	}
+	defer conn.Close()
+
+	client := pb.NewNodeClient(conn)
+
+	res, err := client.Join(ctx, &pb.JoinRequest{
+		Id:   n.ID,
+		Addr: n.PeerAddr,
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to join DHT: %w", err)
+	}
+
+	peers := make(map[string]string)
+
+	for _, peer := range res.GetPeers() {
+		peers[peer.Id] = peer.Addr
+	}
+
+	return peers, nil
+}
