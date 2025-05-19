99ccd0d75e1d57b2103160bb5e24fb4fdffea8b0
Matt Wang <matthewianwang@gmail.com>
2025-05-19 13:43:52 -0700

    chore(log): update the format for log files

--- internal/utils/log_utils.go
+++ internal/utils/log_utils.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 )
@@ -26,11 +27,14 @@ func (lw LogWriter) Write(p []byte) (int, error) {
 }
 
 func SetupLogger(nodeID, peerAddr string) error {
-	if err := os.MkdirAll("logs", 0755); err != nil {
+	date := time.Now().Format("2006-01-02")
+	dir := filepath.Join("logs", nodeID)
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
 		return fmt.Errorf("failed to create logs directory: %w", err)
 	}
 
-	logFilePath := fmt.Sprintf("logs/%s.log", nodeID)
+	logFilePath := filepath.Join(dir, date+".log")
 	f, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
 	if err != nil {
 		return fmt.Errorf("failed to open log file: %w", err)
